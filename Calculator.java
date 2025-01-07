ba3cf9a4d703beb184284a335a9389d414df79aa
Ritika-pal38 <palritika294@gmail.com>
2025-01-07 13:29:32 +0530

    sub uc2_subtraction

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;  
-public class Addition  
+public class Subtraction  
 {  
 public static void main(String args[])  
 {  
-int x, y, sum;  
+int x, y, sub;  
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the first number: ");  
 x = sc.nextInt();  
 System.out.print("Enter the second number: ");  
 y = sc.nextInt();  
 sum = sum(x, y);  
-System.out.println("The sum of two numbers x and y is: " + sum);  
+System.out.println("The subtraction of two numbers x and y is: " - sum);  
 }   
-public static int sum(int a, int b)  
+public static int sub(int a, int b)  
 {  
-int sum = a + b;  
-return sum;  
+int sub = a - b;  
+return sub;  
 }  
 }  
